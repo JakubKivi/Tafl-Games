8655a9c2fa74e7bad90df194fb6583693d6067a6
jakubkivi <panmcleon@gmail.com>
2020-09-11 20:03:03 +0200

    added 2nd version, have to chage some things in repo

---
+++ v2/js/game/functions.js
@@ -0,0 +1,36 @@
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
+
+function randomInt(min, max) {
+	return min + Math.floor((max - min) * Math.random());
+}
+
+window.onresize = function(){
+    s.init(document.getElementById("game"));
+    if(s.w()>s.h()){
+        fieldSize = (s.h()-margin)/size;
+    }else{
+
+        fieldSize = (s.w()-margin)/size;
+    }
+    center = {
+        x: (s.w()-fieldSize)/2,
+        y: (s.h()-fieldSize)/2
+    };
+    startCord = {
+        x: center.x-(Math.trunc(size/2)*fieldSize),
+        y: center.y-(Math.trunc(size/2)*fieldSize)
+    };
+    mouseX=-1000;
+    mouseY=-1000;
+}
---
+++ v2/js/game/movingMechanics.js
@@ -0,0 +1,62 @@
+function move(t, a, b, c, d, e){
+    if(t[a][b]==3)escaping(c, d);
+    var buf=t[a][b];
+    if(buf==3 && a==parseInt(size/2)+1 && b==parseInt(size/2)+1)t[a][b]=5;
+    else t[a][b]=0;
+    t[c][d]=buf;
+    if(c-1>0)if(e==1 &&(t[c+1][d]==3 || t[c-1][d]==3 ||
+                    t[c][d+1]==3 || t[c][d-1]==3)
+    )removeKing(t, c, d);
+    remove(t, c, d, e);
+    
+    
+}
+
+function canMove(t,x,y,tx,ty){
+    if(winCondition=='corner'){
+        if(tx==size && ty==size)return false;
+        if(tx==1 && ty==size)return false;
+        if(tx==size && ty==1)return false;
+        if(tx==1 && ty==1)return false;
+    }
+    if(x!=tx&&y!=ty)return false;
+    if(t[x][y]!=3 && (t[tx][ty]==5||t[x][y]==4))return false;
+
+    if(winCondition=='corner'){
+        if(tx==1 && ty==1)return false;
+        if(tx==1 && ty==size)return false;
+        if(tx==size && ty==1)return false;
+        if(tx==size && ty==size)return false;
+    }
+
+    if(x==tx){
+        if(ty>y){
+            for(var i=y+1; i<=ty; i++){
+                if(t[x][i]==1 || t[x][i]==2 || t[x][i]==3)return false;
+                if(t[x][y]!=3 && t[x][i]==4)return false;
+                if(moveThroughtThrone=='disabled' && t[x][i]==5)return false;
+            }
+        }else if(ty<y){
+            for(var i=y-1; i>=ty; i--){
+                if(t[x][i]==1 || t[x][i]==2 || t[x][i]==3)return false;
+                if(t[x][y]!=3 && t[x][i]==4)return false;
+                if(moveThroughtThrone=='disabled' && t[x][i]==5)return false;
+            }
+        }
+    }else if(y==ty){
+        if(tx>x){
+            for(var i=x+1; i<=tx; i++){
+                if(t[i][y]==1 || t[i][y]==2 || t[i][y]==3)return false;
+                if(t[x][y]!=3 && t[x][i]==4)return false;
+                if(moveThroughtThrone=='disabled' && t[i][y]==5)return false;
+            }
+        }else if(tx<x){
+            for(var i=x-1; i>=tx; i--){
+                if(t[i][y]==1 || t[i][y]==2 || t[i][y]==3)return false;
+                if(t[x][y]!=3 && t[x][i]==4)return false;
+                if(moveThroughtThrone=='disabled' && t[i][y]==5)return false;
+            }
+        }
+    }
+    return true;
+}
