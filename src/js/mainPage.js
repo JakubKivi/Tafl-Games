0363a159dd117456cd44fe1fe748c472a6b254f1
jakubkivi <panmcleon@gmail.com>
2020-10-25 16:12:30 +0100

    options game choosing looks good

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -36,6 +36,14 @@ $(function () {
   $('[data-toggle="popover"]').popover()
 })
 
+$('body').on('click', function (e) {
+    $('[data-toggle=popover]').each(function () {
+        // hide any open popovers when the anywhere else in the body is clicked
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
 
 
 btn.onclick = function() {
